cdbebbf277b3ea08605362a52f55eea5b4e2165d
yejin1024 <yejin1024@gmail.com>
2025-12-31 10:54:30 +0900

    Update get_token.py

--- scripts/get_token.py
+++ scripts/get_token.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
